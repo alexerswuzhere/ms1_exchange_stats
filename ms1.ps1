d49d665b6d02ce08dec4e1e708fe82cf1c54dcfc
Ерсанов Алексей <alexerswuzhere@gmail.com>
2023-11-14 16:41:29 +0300

    add get procent function

--- ms1.ps1
+++ ms1.ps1
@@ -1,8 +1,8 @@
-$procent = 40
-
+$procent = 90
 function Get_Procent {
   param ($_item_size,$_total_item_size)
-  $_return = 
+  $_return = ($_item_size * 100) / $_total_item_size
+  return [math]::Round($_return)
 }
 
 
@@ -15,15 +15,15 @@ function Obj_to_int64 {
 }
 
 
-$e = ForEach-Object {get-mailbox -resultsize 50}
+$e = ForEach-Object {get-mailbox -ResultSize Unlimited}
 foreach ($i in $e)
 {
   $alias = $i.Alias
+
   $get_total_item_size = obj_to_int64 -parameterobject ((get-mailboxstatistics $alias).TotalItemSize.value)
   $ProhibitSendQuota = $null
   if ($i.ProhibitSendQuota -eq "Unlimited"){$ProhibitSendQuota = (get-mailboxstatistics $alias).DatabaseProhibitSendQuota.value} else {$ProhibitSendQuota = $i.ProhibitSendQuota}
   $ProhibitSendQuota = obj_to_int64 -parameterobject $ProhibitSendQuota
-  Write-output $alias,$get_total_item_size, $ProhibitSendQuota,$get_total_item_size.Gettype().Fullname,$ProhibitSendQuota.Gettype().Fullname
-  if ($get_total_item_size -lt $ProhibitSendQuota){Write-Output "TRUE"} else {Write-Output "false"}
+  $alert_message = $alias + " ALERT!"
+  if ((Get_Procent -_item_size $get_total_item_size -_total_item_size $ProhibitSendQuota) -ge $procent) {Write-Output $alert_message}
 }
-
--- telegram_test.ps1
+++
@@ -1,3 +0,0 @@
-Get-Content /home/kali/Desktop/ms1/sensitive.ps1 | ForEach-Object {$ExecutionContext.InvokeCommand.ExpandString($_)}
-$message="Тестовое сообщение в Telegram из PowerShell"
-$Response = Invoke-RestMethod -Uri "https://api.telegram.org/bot$($Telegramtoken)/sendMessage?chat_id=$($Telegramchatid)&text=$($Message)"
